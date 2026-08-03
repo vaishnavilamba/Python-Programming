a1eb5e214896381a4338aefe893de4be31237e28
Vaishnavi Lamba <vaishnavilamba6@gmail.com>
2026-08-03 19:10:00 +0530

    Learn and Work on Modules and Packages. Also do some Practise with some realworld examples

---
+++ 8-ModulesAndPackages/5.2-test.py
@@ -0,0 +1,5 @@
+from package.math import *
+
+
+print(addition(2,3))
+print(substraction(6,7))
---
+++ 8-ModulesAndPackages/package/_init_.py
@@ -0,0 +1,3 @@
+"""
+_init_.py is a special file used in python to define packages and initialze their namespaces. 
+"""
